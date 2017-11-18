168c150b86d53ec392efee3e3305f24070c0a28d
Joao Santana <wozefacu@cartelera.org>
2017-11-18 08:41:59 -0200

    sequencia do projeto

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,3 +1,46 @@
-/**
- * 
- */
+//retorna numero, texto ou boolean
+
+function soma(n1 , n2){
+	return n1 + n2;
+}
+
+function subtracao(n1 , n2){
+	return n1 - n2;
+}
+
+function multiplicacao(n1, n2){
+	return n1 * n2;
+}
+
+function divisao(n1, n2){
+ try{
+	 if (n2==0){
+		 throw "Error";
+	 }
+	 return n1/n2;
+	 
+ }catch(e){
+	 alert(e);
+ }
+ 
+}
+
+
+
+function operacao(op,n1,n2){
+	
+	 switch(op){
+	 case  "soma" : 
+		 alert("ate aqui foi");
+      document.getElementById("resposta").innerHtml =   soma(n1,n2);
+      alert("certo ...");
+	  break;
+	  
+	 default : 
+	   document.getElementById("resposta").innerHtml =   "Nao Encontrado...";
+	  break;
+	 }
+		
+}
+
+
---
+++ WebContent/sistema.jsp
@@ -0,0 +1,12 @@
+<script src="js/script.js"></script>
+
+<form name="form1" id="form1">
+<br/>
+
+
+<button id="btn1" ></button>
+</form>
+
+<div id="resposta"></div>
+
+
